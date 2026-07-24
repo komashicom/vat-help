52204a96c205beda4b8707ed42379dd9d13856bb
komashicom <info@komashi.com>
2026-07-24 10:54:44 +0200

    add workflow

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./i18n";
 import "./index.css";
+import { setResultsUrl } from "./lib/vat";
+
+setResultsUrl(`${import.meta.env.BASE_URL}data/results.json`);
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
