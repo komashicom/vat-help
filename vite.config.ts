bbd349661af1f65d628ed887efcd97dcb4ba4fa0
Komashi Official <info@komashi.com>
2026-07-24 10:31:42 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwind()],
+  base: '/vat-help/',
   resolve: {
     alias: { "@": fileURLToPath(new URL("./src", import.meta.url)) },
   },
